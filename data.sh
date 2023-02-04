31fa73c582fd8952d0c09bd94d4d14a1dab81690
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-04 23:53:49 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get full upgrade -y
+sudo apt-get install python3-pip -y
+sudo apt-get install pkg-config -y
+
+git clone https://github.com/shalu212/Agri.git
+cd Agri
+pip3 install -r requirements.txt
+python3 app.py &
+git clone https://github.com/shalu212/flight-perdiction.git
+cd flight-perdiction
+pip3 install -r requirements.txt
+python3 app.py &
+git clone https://github.com/shalu212/Medical-Insurance.git
+cd /Medical-Insurance
+pip3 install -r requirements.txt
+python3 app.py &
