e74d5ceb99c4a755605d173a0967dc5204a44f48
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-05 12:58:57 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -2,11 +2,10 @@
 sudo apt update
 sudo apt-get full upgrade -y
 sudo apt-get install python3-pip -y
-sudo apt-get install pkg-config -y
 git clone https://github.com/shalu212/Agri.git
 cd Agri
 pip3 install -r requirements.txt
-python3 app.py 
+python3 app.py &
 git clone https://github.com/shalu212/flight-perdiction.git
 cd flight-perdiction
 pip3 install -r requirements.txt
